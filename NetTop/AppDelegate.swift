7c6e25c36c8b3c837c37e0237e9065ab9b4ddccd
huhuanming <workboring@gmail.com>
2017-10-22 18:29:54 +0800

    Record NetWork Traffic

--- NetTop/AppDelegate.swift
+++ NetTop/AppDelegate.swift
@@ -7,12 +7,11 @@
 //
 
 import Cocoa
+import ApplicationServices
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
@@ -20,7 +19,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
 
 }
 
--- NetTop/ViewController.swift
+++ NetTop/ViewController.swift
@@ -9,18 +9,39 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateNetWorkData), userInfo: nil, repeats: true)
-        RunLoop.current.run()
+    
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    @IBOutlet var statusMenu: NSMenu!
+    
+    @IBAction func quitClick(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
     }
     
-    func updateNetWorkData() {
+    
+    var applicationMap: Dictionary<String, NTApplication> = [:]
+    
+    var processMap: Dictionary<String, String> = [:]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        statusItem.title = "NetTop"
+        statusItem.menu = statusMenu
+
+        for app in NSWorkspace.shared().runningApplications {
+            if app.bundleIdentifier != nil {
+                let bundleIdentifier = app.bundleIdentifier ?? ""
+                self.applicationMap[bundleIdentifier] = NTApplication(bundleIdentifier: bundleIdentifier, name: app.localizedName ?? bundleIdentifier, icon: app.icon!, bytesIn: 0, bytesOut: 0)
+                self.processMap[String(app.processIdentifier)] = bundleIdentifier
+            }
+        }
+        self.updateTrafficData()
+    }
+    
+    func updateTrafficData() {
         let task = Process()
         task.launchPath = "/usr/bin/nettop"
-        task.arguments = ["-x", "-k", "state", "-k", "interface", "-k", "rx_dupe", "-k", "rx_ooo", "-k", "re-tx", "-k", "rtt_avg", "-k", "rcvsize", "-k", "tx_win", "-k", "tc_class", "-k", "tc_mgt", "-k", "cc_algo", "-k", "P", "-k", "C", "-k", "R", "-k", "W", "-l", "1", "-t", "wifi", "-t", "wired"]
+        task.arguments = ["-t", "wifi", "-t", "wired", "-P", "-L", "1"]
         
         let pipe = Pipe()
         task.standardOutput = pipe
@@ -34,7 +55,36 @@ class ViewController: NSViewController {
             let output = pipe.fileHandleForReading.availableData
             let outputString = String(data: output, encoding: String.Encoding.utf8) ?? ""
             
-            print(outputString)
+            let stringArray = outputString.split(separator: "\n")
+            
+            if stringArray.count < 2 {
+                DispatchQueue.main.async {
+                    self.updateTrafficData()
+                }
+                return
+            }
+            
+            
+            self.statusMenu.removeAllItems()
+            
+            for i in 1...(stringArray.count-1) {
+                let string = stringArray[i]
+                let infos = string.components(separatedBy: ",")
+                let processId = infos[1].components(separatedBy: ".").last!
+                let bytesIn = infos[4]
+                let bytesOut = infos[5]
+                
+                if let bundleIdentifier = self.processMap[processId] {
+                    if let app = self.applicationMap[bundleIdentifier] {
+                        let menuItem = NSMenuItem.init(title: "\(app.name) \(Float(bytesIn)! / 1024 / 1024) Mib \(Float(bytesOut)! / 1024 / 1024) Mib", action: nil, keyEquivalent: "")
+                        self.statusMenu.addItem(menuItem)
+                    }
+                }
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2, execute: {
+                self.updateTrafficData()
+            })
         }
     }
     
